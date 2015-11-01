270104e674c2be893e9eb6d8d8042adf5379134d
Alexandru CIOALAI <acioalai@e1p62.cluj.42.fr>
2015-11-01 17:15:03 +0100

    first push

---
+++ ft_memset.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acioalai <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/31 08:10:31 by acioalai          #+#    #+#             */
+/*   Updated: 2015/10/31 10:45:19 by acioalai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "header.h"
+
+void *ft_memset(void *b, int c, size_t len)
+{
+	size_t	i;
+	unsigned char	*temp;
+	
+	if (len == 0)
+		return(b);
+	temp = (unsigned char*) b;
+	i = 0;
+	printf("before while\n");
+	while (i <= len)
+	{
+		temp[i] = (unsigned char) c;
+		i++;
+	}
+	return(b);
+}
---
+++ ft_strlcat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acioalai <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/25 11:53:19 by acioalai          #+#    #+#             */
+/*   Updated: 2015/10/25 16:57:27 by acioalai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "header.h"
+/*
+   size_t	ft_strlcat(char *dst, const char *src, size_t size)
+   {
+   return (ft_strlen(ft_strncat(dst, src, size)));
+   size_t	len; 
+
+   len = strlen(dst);
+   if (len < size - 1)
+   return(len + strlcpy(dst + len,src, size - len));
+   else
+   return(len + strlen(src));
+   }*/
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	s1_end;
+
+	i = 0;
+	while (dst[i] && i < size)
+		i++;
+	s1_end = i;
+	while (src[i - s1_end] && i < size - 1)
+	{
+		dst[i] = src[i - s1_end];
+		i++;
+	}
+	if (s1_end < size)
+		dst[i] = '\0';
+	return (s1_end + ft_strlen(src));
+}
