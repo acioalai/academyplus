f4c08c42405c9be0ecf3aed7487732d639466b68
Alexandru CIOALAI <acioalai@e1p67.cluj.42.fr>
2015-11-03 18:27:52 +0100

    memmove

---
+++ ft_memmove.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acioalai <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/03 17:49:52 by acioalai          #+#    #+#             */
+/*   Updated: 2015/11/03 18:26:34 by acioalai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "header.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+	unsigned char	*temp;
+		
+	temp = (void *)malloc(len);
+	i = 0;
+	while (i < len)
+	{
+		*(temp + i) = *((unsigned char *)src + i);
+		i++;
+	}
+	ft_memcpy(dst, src, len);
+	free (temp);
+	return (dst);
+}
