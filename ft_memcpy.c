d5c9aabc3cb9823f93da1f08b86c05a88d073390
Alexandru CIOALAI <acioalai@e1p51.cluj.42.fr>
2015-11-03 08:04:03 +0100

    memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acioalai <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/03 07:18:35 by acioalai          #+#    #+#             */
+/*   Updated: 2015/11/03 07:43:54 by acioalai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "header.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		*((unsigned char *)dst + i) = *((unsigned char *)src + i); 
+		i++;	
+	}
+	return (dst);
+}
