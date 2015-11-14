c0a12aea7ac6a49837920613149cb14ee094d46e
Alexandru CIOALAI <acioalai@e1p49.cluj.42.fr>
2015-11-14 15:04:23 +0100

    update

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,12 +6,82 @@
 /*   By: acioalai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/10 08:52:13 by acioalai          #+#    #+#             */
-/*   Updated: 2015/11/10 08:55:14 by acioalai         ###   ########.fr       */
+/*   Updated: 2015/11/14 15:03:42 by acioalai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int		ft_words_nbr(const char *s, char c)
+{
+	int		nbr;
+	int		i;
+	int		k;
+
+	nbr = 0;
+	while (s[i])
+	{
+		while ((s[i] != '\0') && (s[i] == c))
+			i++;
+		k = 0;
+		while ((s[i] != '\0') && (s[i] != c))
+		{
+			i++;
+			k++;
+		}
+		if (k != 0)
+			nbr++;
+	}
+	return (nbr);
+}
+
+char	**ft_malloc(const char *s,	int nbr)
+{
+	char	**str;
+	int		i;
+	puts("MALLOC");
+	str = (char **)malloc(sizeof(char *) * (ft_strlen(s) + 1));
+	while (str[i])
+	{
+		str[i] = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+		i++;	
+	}
+	return(str);
+}
+
 char	**ft_strsplit(char const *s, char c)
 {
-	
-	return ()
+	char	**matrix;
+	int		i;
+	int		k;
+	int		word_nbr;
+	int		p;
+
+
+	if (s == NULL)
+		return(NULL);
+	word_nbr = ft_words_nbr(s, c);
+
+	printf("%d", word_nbr);
+	matrix = ft_malloc(s, word_nbr);
+	p = 0;
+	k = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		while ((s[i] == c) && (s[i] != '\0'))
+			i++;
+		matrix[p][k] = s[i];
+		printf(" s[i] = %c", s[i]);
+		i++;
+		k++;
+		
+		if (s[i] == c)
+		{
+			matrix[p][k] = '\0';
+			p++;
+			k = 0;
+		}
+	}
+	return(matrix);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: acioalai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/04 07:29:17 by acioalai          #+#    #+#             */
-/*   Updated: 2015/11/10 08:36:56 by acioalai         ###   ########.fr       */
+/*   Updated: 2015/11/14 14:25:38 by acioalai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,40 @@
 
 int		main(void)
 {
-/*	char *s1,*s2;
+	/*	char *s1,*s2;
 
-	strcpy(s1,"abc");
-	strcpy(s2,"abc");
-	printf("%d", ft_strequ(s1,s2));	
+		strcpy(s1,"abc");
+		strcpy(s2,"abc");
+		printf("%d", ft_strequ(s1,s2));	
 
-	void	*str;
-	puts(ft_memchr("abcdef",'a' , 5));
-	
-	puts(memchr("abcdef",'a' , 5));
-	
-	char	*s;
-	ft_memmove(s, "acasa", 2);
-	puts(s);	
-	puts(memmove(s, "acasa", 2));
+		void	*str;
+		puts(ft_memchr("abcdef",'a' , 5));
 
-	puts(ft_strjoin("afara ","este frig"));
-	printf("\n%zu", ft_strlen(ft_strjoin("afara ","este frig")));
-*/
-	puts("               acasa         ");
-	puts(ft_strtrim("               acasa       i  "));
+		puts(memchr("abcdef",'a' , 5));
+
+		char	*s;
+		ft_memmove(s, "acasa", 2);
+		puts(s);	
+		puts(memmove(s, "acasa", 2));
+
+		puts(ft_strjoin("afara ","este frig"));
+		printf("\n%zu", ft_strlen(ft_strjoin("afara ","este frig")));
+
+		puts("               acasa         ");
+		puts(ft_strtrim("               acasa       i  "));
+		*/
+	char	s[100] = "ASD*AD*AS*DA*";
+	char	**matrix;
+	int		i,n;
+
+	puts(s);
+	matrix = ft_strsplit(s, '*');
+	i = 0;
+	while (matrix[i])
+	{
+		puts(matrix[i]);
+		i++;
+	}	
 
 
 
